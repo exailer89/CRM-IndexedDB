5021abe08f372b823943efc2df9171a740d202f9
Hugo Barrera <exailer89@gmail.com>
2024-02-18 22:32:12 -0600

    Agregando Nuevos Clientes en la BD

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -35,6 +35,38 @@
 
             return;
         }
+
+        // Crear un objeto con la información
+        const cliente = {
+            nombre : nombre,
+            email : email,
+            telefono : telefono,
+            empresa : empresa,
+        }
+
+        cliente.id = Date.now();
+        
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'], 'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error', 'error');
+        };
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('El Cliente se agrego correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        }
     }
 
     function imprimirAlerta(mensaje, tipo) {

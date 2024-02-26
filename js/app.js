2feab5ac2e762abc2b51433f9c1bd5ccb46060ff
Hugo Barrera <exailer89@gmail.com>
2024-02-25 23:07:05 -0600

    Eliminar Clientes de la BD

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function(){
 
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -8,7 +9,33 @@
         if (window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro);
     });
+
+    function eliminarRegistro(e) {
+        if(e.target.classList.contains('eliminar')) {
+            const idEliminar = Number(e.target.dataset.cliente);
+            const confirmar = confirm('¿Deseas eliminar este cliente?');
+
+            if (confirmar) {
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function() {
+                    console.log('Eliminando...');
+
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function() {
+                    console.log('Hubo un error');
+                }
+            }
+        }
+    }
 
     // Crea la base de datos de IndexDB
     function crearDB() {
@@ -54,9 +81,8 @@
 
                 if(cursor) {
                     const { nombre, empresa, email, telefono, id } = cursor.value; 
-                    const ListadoClientes = document.querySelector('#listado-clientes');
-
-                    ListadoClientes.innerHTML += `
+                    
+                    listadoClientes.innerHTML += `
                         <tr>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                 <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
@@ -70,7 +96,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;

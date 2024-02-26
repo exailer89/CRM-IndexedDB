729e0a7d83c2c99792c5a194496a07a81d6f8bda
Hugo Barrera <exailer89@gmail.com>
2024-02-25 22:46:22 -0600

    Llenar el Formulario con la información del Cliente

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,21 +1,66 @@
 (function() {
 
     let DB;
+    let idCliente;
 
     const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+
+    const formulario = document.querySelector('#formulario');
 
     document.addEventListener('DOMContentLoaded', () => {
         conectarDB();
 
+        // Actualiza el registro
+        formulario.addEventListener('submit', actualizarCliente);
+
         // Verificar el ID de la URL
         const parametrosURL = new URLSearchParams(window.location.search); // window.location.search nos da solo el "Query String"
-        const idCliente = parametrosURL.get('id');
+        idCliente = parametrosURL.get('id');
         if (idCliente) {
             setTimeout(() => {
                 obtenerCliente(idCliente);
             }, 100); // Colocamos un setTimeout para corregir un error de asincronizmo, más adelante del curso, veremos clases especificas de este tema.
         }
     });
+
+    function actualizarCliente(e) {
+        e.preventDefault();
+        
+        if (nombreInput.value === '' || emailInput.value === '' || telefonoInput.value === '' || empresaInput.value === '') {
+            imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+
+        // Actualizar cliente
+        const clienteActualizado = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: Number(idCliente),
+        }
+
+        const transaction = DB.transaction(['crm'], 'readwrite');
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.put(clienteActualizado);
+
+        transaction.oncomplete = function() {
+            imprimirAlerta('Editado correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 3000);
+        };
+
+        transaction.onerror = function() {
+            imprimirAlerta('Hubo un error al editar', 'error');
+        }
+    }
 
     function obtenerCliente(id) {
         const transaction = DB.transaction(['crm'], 'readwrite');
@@ -36,9 +81,12 @@
     }
 
     function llenarFormulario(datosCliente) {
-        const { nombre } = datosCliente;
+        const { nombre, email, telefono, empresa } = datosCliente;
 
         nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
     }
 
     function conectarDB() {
